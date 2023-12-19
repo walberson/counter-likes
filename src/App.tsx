ba096beeb6b5953c41409c2d82060b60dd1a0b27
Walberson Dias <walberson.mv@gmail.com>
2023-12-18 21:52:42 -0300

    att

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [likes, setLikes] = useState(0);
+  const [comments, setComments] = useState("");
+  const [lista, setLista] = useState<string[]>([]);
+
+  useEffect(() => {
+    console.log("likes mudou");
+    if (likes > 10) {
+      setLista((listaAnterior: string[]) =>
+        listaAnterior.concat("Famosinho!")
+      );
+    }
+  }, [likes]);
+
+  function somaLikes() {
+    setLikes((ultimoValor) => ultimoValor + 1);
+  }
+
+  function adicionarLista() {
+    setLista((listaAnterior: string[]) => listaAnterior.concat(comments));
+  }
+
   return (
     <div className="p-10">
       <div className="flex flex-col justify-evenly p-4 w-96 h-96 bg-slate-400 rounded-3xl">
@@ -25,16 +45,24 @@ function App() {
             className="w-full h-56 object-cover rounded-2xl"
           />
         </div>
+
         <div className="flex justify-between text-xl">
-          <button
-            className="cursor-pointer"
-            onClick={() => setLikes(likes + 1)}
-          >
-            <span>❤️ {likes} curtidas</span>
+          <button className="cursor-pointer" onClick={somaLikes}>
+            {likes < 10 ? (
+              <span>❤️ {likes} curtidas</span>
+            ) : (
+              <span>❤️ Famosinho!</span>
+            )}
           </button>
           <span>🚀 Compartilhar</span>
         </div>
       </div>
+      <input
+        type="text"
+        onChange={(event) => setComments(event.target.value)}
+      />
+      <button onClick={adicionarLista}>adicionar a lista</button>
+      <span>{lista}</span>
     </div>
   );
 }
